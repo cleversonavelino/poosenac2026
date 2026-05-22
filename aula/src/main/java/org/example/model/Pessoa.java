505fb68415d829dc908eb5f2bd4648f527aa073c
Avelino <cleverson.avelino@dell.com>
2026-05-21 21:55:07 -0300

    first commit

---
+++ aula/src/main/java/org/example/model/Pessoa.java
@@ -0,0 +1,23 @@
+package org.example.model;
+
+public class Pessoa {
+    //wrappers
+    private int idade;
+    private boolean dormindo;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isDormindo() {
+        return dormindo;
+    }
+
+    public void setDormindo(boolean dormindo) {
+        this.dormindo = dormindo;
+    }
+}
