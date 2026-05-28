2758996f6cb9d35129f80d169e512dedc971747f
cferreira.ext <cferreira.ext>
2026-05-27 21:19:14 -0300

    hibernate

---
+++ aula/src/main/java/senac/aula/App.java
@@ -0,0 +1,40 @@
+package senac.aula;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import senac.aula.model.Cliente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //crio a fábrica de entity manager
+        //um entity manager gerencia as entidades
+        //ex: cliente
+        //com ele é possível salvar alterar listar dados do banco
+        EntityManagerFactory emf = Persistence.
+        		createEntityManagerFactory("aula-senac-poo");
+        //recupero um entity manager para poder fazer operações de banco
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        // cria o objeto do tipo cliente
+        Cliente cliente = new Cliente();
+        cliente.setNome("meu nome");
+        cliente.setSenha("123456");
+        //método utilizado para salvar o objeto no banco de dados
+        em.persist(cliente);
+
+        em.getTransaction().commit();
+        
+        em.close();
+        emf.close();
+    }
+}
---
+++ aula/src/main/java/senac/aula/model/Cliente.java
@@ -0,0 +1,43 @@
+package senac.aula.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="CLIENTE")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String nome;
+	private String senha;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
