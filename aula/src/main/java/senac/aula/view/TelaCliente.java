ba23ed0c1ef5d30cbf3abd2dc8fcaa0e3894d67d
cferreira.ext <cferreira.ext>
2026-06-10 18:53:55 -0300

    swing

---
+++ aula/src/main/java/senac/aula/view/TelaCliente.java
@@ -0,0 +1,62 @@
+package senac.aula.view;
+
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import senac.aula.model.Cliente;
+
+public class TelaCliente extends JInternalFrame {
+
+	public TelaCliente() {
+		setTitle("Cadastro de Clientes");
+
+		setClosable(true);
+		setMaximizable(true);
+
+		setSize(800, 600);
+
+		setLayout(null);
+
+		JLabel labelNome = new JLabel("Nome:");
+		labelNome.setBounds(30, 30, 80, 25); // x, y, largura, altura
+		add(labelNome);
+
+		// Campo Nome
+		JTextField campoNome = new JTextField();
+		campoNome.setBounds(120, 30, 180, 25);
+		add(campoNome);
+
+		// Label Senha
+		JLabel labelSenha = new JLabel("Senha:");
+		labelSenha.setBounds(30, 70, 80, 25);
+		add(labelSenha);
+
+		// Campo Senha
+		JPasswordField campoSenha = new JPasswordField();
+		campoSenha.setBounds(120, 70, 180, 25);
+		add(campoSenha);
+		
+		 // Botão Salvar
+        JButton botaoSalvar = new JButton("Salvar");
+        botaoSalvar.setBounds(120, 110, 100, 30);
+        add(botaoSalvar);
+        
+        botaoSalvar.addActionListener(e -> {
+        	System.out.println("clicou no salvar");
+        	Cliente cliente = new Cliente();
+        	
+        	cliente.setNome(campoNome.getText());
+        	cliente.setSenha(new String(campoSenha.getPassword()));
+        	
+        	System.out.println(cliente.getNome());
+        	System.out.println(cliente.getSenha());
+        	
+        });
+
+		setVisible(true);
+	}
+
+}
---
+++ aula/src/main/java/senac/aula/view/TelaPrincipal.java
@@ -0,0 +1,61 @@
+package senac.aula.view;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class TelaPrincipal extends JFrame {
+
+	public TelaPrincipal() {
+		// Configura o título da janela
+		setTitle("Projeto Aula");
+
+		// Define o tamanho da janela (largura x altura)
+		setSize(1024, 768);
+
+		// Posiciona a janela no centro da tela
+		setLocationRelativeTo(null);
+
+		// Define a operação padrão ao fechar a janela
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+
+		// Cria um DesktopPane (área para internal frames)
+		JDesktopPane desktopPane = new JDesktopPane();
+		add(desktopPane);
+
+		// Cria a barra de menu
+		JMenuBar menuBar = new JMenuBar();
+
+		// Cria o menu "Arquivo"
+		JMenu menuCadastros = new JMenu("Cadastros");
+
+		JMenuItem itemCliente = new JMenuItem("Clientes");
+		itemCliente.addActionListener(e -> {
+			desktopPane.add(new TelaCliente());
+		});
+
+		// Cria o item "Sair" dentro do menu
+		JMenuItem itemSair = new JMenuItem("Sair");
+
+		// Ação ao clicar em Sair
+		itemSair.addActionListener(e -> {
+			System.exit(0);
+		});
+
+		// Adiciona o item ao menu
+		menuCadastros.add(itemCliente);
+		menuCadastros.add(itemSair);
+
+		// Adiciona o menu à barra de menu
+		menuBar.add(menuCadastros);
+
+		// Define a barra de menu na janela
+		setJMenuBar(menuBar);
+
+		// Torna a janela visível
+		setVisible(true);
+	}
+
+}
